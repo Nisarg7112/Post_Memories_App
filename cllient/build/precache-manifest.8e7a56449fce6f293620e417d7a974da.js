915b4ef1662738da83fe2fbc8eb7a005562bef68
Nisarg Shah <109804208+Nisarg7112@users.noreply.github.com>
2022-09-03 20:25:02 +0530

    completed

---
+++ cllient/build/precache-manifest.8e7a56449fce6f293620e417d7a974da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1bb68738e0433b76b76938db5df182",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40a54dcc4425763d068a",
+    "url": "/static/css/main.42acf6b3.chunk.css"
+  },
+  {
+    "revision": "41ffd322c56424e58695",
+    "url": "/static/js/2.2b2ff9fa.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.2b2ff9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a54dcc4425763d068a",
+    "url": "/static/js/main.21d0d97a.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
